c7a1cf50fdc9571dad5066b30d1f8470d7e4b2fa
Canh Do Minh <minhcanh99@gmail.com>
2020-11-04 23:04:48 +0900

    Ready for experiment

--- src/main/Checker.java
+++ src/main/Checker.java
@@ -2,11 +2,12 @@ package main;
 
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.JPF;
+import pattern.listener.AnalyzerListener;
 
 public class Checker {
 	
 	public static void main(String[] args) {
-		String testFileName = "TestRace1";
+//		String testFileName = "TestRace1";
 //		String testFileName = "account.Main"; // P = 0, 16s
 //		String testFileName = "airline.Main"; // P = 0, 1s
 //		String testFileName = "alarmclock.AlarmClock"; // P = 0, 1s
@@ -24,14 +25,14 @@ public class Checker {
 //		String testFileName = "SimpleTest.Main"; // P = 5, 0s
 //		String testFileName = "twoStage.Main"; // P = 26, 0s
 //		String testFileName = "org.apache.log4j.TestThrowableStrRep"; // P = 5, cannot detect bug
-//		String testFileName = "wronglock2.Main"; // P = 11, 0s
+		String testFileName = "wronglock2.Main"; // P = 11, 0s
         String[] str = new String[]{
             "+search.class=pattern.search.BestFirstSearch",
             testFileName};
 	    Config config = new Config(str);
         JPF jpf = new JPF(config);
-//        SimpleDot2 listener2 = new SimpleDot2(config, jpf);
-//        jpf.addListener(listener2);
+        AnalyzerListener listener = new AnalyzerListener();
+        jpf.addListener(listener);
         jpf.run();
 	}
 }
---
+++ src/pattern/listener/AnalyzerListener.java
@@ -0,0 +1,28 @@
+package pattern.listener;
+
+import gov.nasa.jpf.ListenerAdapter;
+import gov.nasa.jpf.search.Search;
+
+public class AnalyzerListener extends ListenerAdapter {
+	
+	int count;
+	
+	public AnalyzerListener() {}
+
+	@Override
+	public void stateAdvanced(Search search) {
+		if (search.isEndState()) {
+			count ++;
+		}
+	}
+
+	@Override
+	public void searchStarted(Search search) {
+		count = 0;
+	}
+
+	@Override
+	public void searchFinished(Search search) {
+		System.out.println("P-Measure = " + count);
+	}
+}
